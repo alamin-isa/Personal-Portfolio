652e1b21362a919ece5995728157af6cedc315b5
Alamin_isa <alaminissa200@gmail.com>
2025-03-19 13:05:19 +0100

    alamin's portfollio

---
+++ index.js
@@ -0,0 +1,27 @@
+// (function ($) {
+//   "use strict";
+
+// // $(window).scroll(function (){
+// //     if ($(this).scrollTop() > 200){
+// //         $('.navbar').fadeIn('slow').css('display', 'flex');
+    
+// //     }else{
+// //         $('.navbar').fadeOut('slow').css('display', 'none');
+// //     }
+// // }
+// // )
+
+// //   // type initiate
+// //   if ($(".typed-text-output").length == 1) {
+// //     var typed_strings = $(".typed-text").text();
+// //     var typed = new Typed(".typed-text-output", {
+// //       strings: typed_string.split(", "),
+// //       typeSpeed: 100,
+// //       backSpeed: 30,
+// //       smartBackspace: false,
+// //       loop: true
+// //     });
+// //   }
+// })(jQuery);
+
+
